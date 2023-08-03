078ab19003460a11bd7bd08cc73e0b5f2f5984c3
tom4649 <tkaaynakiyoshi@gmail.com>
2023-08-03 09:01:16 +0900

    implement http server

--- docker/filter-server/src/server.py
+++ docker/filter-server/src/server.py
@@ -5,6 +5,7 @@ from starkware.cairo.common.hash_chain import (compute_hash_chain)
 
 import argparse
 from http.server import (HTTPServer, BaseHTTPRequestHandler)
+import json
 
 def parse_args():
     parser = argparse.ArgumentParser(description='run server with options.')
@@ -13,67 +14,69 @@ def parse_args():
     args = parser.parse_args()
     return args
 
+filter_words = []
+
 class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def __init__(self, *args, **kwargs):
+        global filter_words
+
         self.args = parse_args()
 
-        config_file = args.config
+        config_file = self.args.config
         d = {}
         with open(config_file) as f:
-            d = json.loads(f)
+            d = json.load(f)
         if "filter_words" in d:
-            self.filter_words = d["filter_words"]
-        else:
-            self.filter_words = []
-
+            filter_words = list(set(filter_words + (d["filter_words"])))
         if "password" in d:
             self.password = d["password"]
         else:
             self.password = ""
-
-
         super().__init__(*args, **kwargs)
 
-    # return list of filtered words, signed with admin private key
     def do_GET(self):
+        global filter_words
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        # sign and return json
-        json_str = json.dumps(self.filter_words)
+        json_str = json.dumps(filter_words)
         signature = ""
         pubkey = ""
         ret = json.dumps({"pubkey": pubkey, "signature_r": signature, "signature_s": signature, "json_str": json_str})
-        self.wfile.write(ret)
+        print(ret)
+        self.wfile.write(ret.encode())
 
-    # add filtered words. do not check signature etc. it comes from trusted local source.
-    # endpoint is protected by password.
     def do_POST(self):
-        content_length = int(self.headers['content-length'])
-        body = self.rfile.read(content_length).decode('utf-8')
-        new_word_json = json.loads(body)
-        new_word = new_word_json["word"]
-        if not new_word in self.filter_words:
-            self.filter_words.append(new_word)
-        # check password
-        if "password" in new_word_json and new_word_json["password"] == self.password:
-            pass
-        else:
-            self.send_response(400)
-            return
-        
+        global filter_words
         self.send_response(200)
-        self.send_header('Content-Type', 'application/json')
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(b'{"result": "ok"}')
+        self.wfile.write(b"""
+            <html>
+            <body>
+                <form action="/" method="post">
+                    Enter filter words: <input type="text" name="user_input">
+                    <input type="submit" value="Submit">
+                </form>
+            </body>
+            </html>
+        """)
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        
+        filter_words.append(post_data.decode('utf-8').removeprefix("user_input="))
+        print("filter_words:", filter_words)
+
+
 
 def run_server():
-    httpd = HTTPServer(('0.0.0.0', 8080), CustomHTTPRequestHandler)
+    httpd = HTTPServer(('0.0.0.0', 8000), CustomHTTPRequestHandler)
     httpd.serve_forever()
 
 
 def main():
+    print("Start server")
     run_server()
 
 main()
