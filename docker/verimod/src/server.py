0dd0c0730b5cd084567e4faa568acf2f5631b2a5
tom4649 <tkaaynakiyoshi@gmail.com>
2023-09-14 11:20:59 +0900

    Not completed

--- docker/verimod/src/server.py
+++ docker/verimod/src/server.py
@@ -9,6 +9,10 @@ import argparse
 from http.server import (HTTPServer, BaseHTTPRequestHandler)
 from urllib.parse import (urlparse, parse_qs)
 import json
+import sys
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def parse_args():
     parser = argparse.ArgumentParser(description='run server with options.')
@@ -32,12 +36,14 @@ def check_category_elements_child(pubkey, category_elements_child):
 class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def __init__(self, *args, **kwargs):
-        self.args = parse_args()
-        config_file = self.args.config
-        # @todo make initial state not here, run only once
-        config_file = args.config
-        d = {}
+        logging.info("Initialization")
+        print("Initializing server")
+        sys.stdout.flush()
         try:
+            self.args = parse_args()
+            config_file = self.args.config
+            # @todo make initial state not here, run only once
+            d = {}
             with open(config_file) as f:
                 d = json.load(f)
             self.initial_state = d['initial_state']
@@ -45,44 +51,68 @@ class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
             self.final_state , _ = make_final_state(self.initial_state, self.blocks) 
             self.state = self.initial_state   
         except Exception as e:
+            logging.error(f"Error: No state file found. Exception: {e}")
             print("Error: No state file found")
             raise e
+        print("Initial state: ", self.initial_state)
         super().__init__(*args, **kwargs)
 
     # check if requested data satisfies condition
     def do_GET(self):
-
-        # query state and respond here
-
-        parsed = urlparse(self.path)
-        params = parse_qs(parsed.query)
-
-        category_type = params["category_type"][0]
-        pubkey = params["pubkey"][0]
-
-        result = False
-        for category in self.state["all_category"]:
-            if category["data"]["category_type"] == category_type:
-                category_elements_child = category["data"]["category_elements_child"]
-                exists = check_category_elements_child(pubkey, category_elements_child)
-                if exists:
-                    result = True
-                    break
-
+        logging.info("Received GET request.")
         self.send_response(200)
-        self.send_header('Content-type', 'application/json')
+        self.send_header('Content-type', 'text/plain; charset=utf-8')
         self.end_headers()
+        # self.wfile.write('GETメソッドを実装'.encode())
+        print("self.path:", self.path)
+        sys.stdout.flush()
+        # query state and respond here
+        try:
+            parsed = urlparse(self.path)
+            params = parse_qs(parsed.query)
+
+            category_type = params["category_type"][0]
+            pubkey = params["pubkey"][0]
+
+            result = False
+            for category in self.state["state"]["all_category"]:
+                if category["data"]["category_type"] == category_type:
+                    print("category found")
+                    category_elements_child = category["data"]["category_elements_child"]
+                    exists = check_category_elements_child(pubkey, category_elements_child)
+                    if exists:
+                        result = True
+                        print("Correct category exits")
+                        break
+                    else:
+                        print("Correct category does not exit")
+
+        except Exception as e:
+            logging.error(f"Error: No state file found. Exception: {e}")
+            print("Error: No state file found")
+            raise e
+            pass
+        # self.send_header('Content-type', 'application/json')
+        # self.end_headers()
         ret = json.dumps({"result": result})
         self.wfile.write(ret.encode('utf-8'))
 
     # add block data to memory
     def do_POST(self):
-
+        print("POST")
+        logging.info("Received Post request.")
+        sys.stdout.flush()
         # update state by applying blocks here
-        content_length = int(self.headers['content-length'])
-        body = self.rfile.read(content_length).decode('utf-8')
-        new_block_json = json.loads(body)
-        new_block = new_block_json["block"]
+        try:
+            content_length = int(self.headers['content-length'])
+            body = self.rfile.read(content_length).decode('utf-8')
+            new_block_json = json.loads(body)
+            new_block = new_block_json["block"]
+        except Exception as e:
+            logging.error(f"Error: No state file found. Exception: {e}")
+            print("Error: Invalid block")
+            raise e
+            pass
 
         try:
             final_state, final_hash = make_final_state(self.state, [new_block])
@@ -91,14 +121,20 @@ class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
 
             # write state to json file
         except Exception as e:
+            logging.error(f"Error: No state file found. Exception: {e}")
+            print("Error: Invalid block")
+            raise e
             pass
 
         self.send_response(200)
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
+        response = {"result": "ok"}
+        self.wfile.write(json.dumps(response).encode('utf-8'))
 
 def run_server():
     httpd = HTTPServer(('0.0.0.0', 8000), CustomHTTPRequestHandler)
+    logging.info("Server started on port 8000")
     print("Server started on")
     httpd.serve_forever()
 
