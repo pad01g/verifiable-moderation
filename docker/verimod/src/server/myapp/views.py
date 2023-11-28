f0af5a54bfa4f04b8ac4f5a7d8b8921fc1dcee90
pad01g <101613202+pad01g@users.noreply.github.com>
2023-11-28 02:07:03 +0000

    make it work with post/get

--- docker/verimod/src/server/myapp/views.py
+++ docker/verimod/src/server/myapp/views.py
@@ -21,9 +21,9 @@ class StateHandler:
                 d = json.load(f)
             self.initial_state = d['initial_state']
             self.blocks = d['blocks']
-            self.final_state, _ = make_final_state(self.initial_state, self.blocks)
-            self.state = [value for key, value in d.items() if "state" in key]
-            self.state.append(self.final_state)
+            self.state, _ = make_final_state(self.initial_state, self.blocks)
+            # self.state = [value for key, value in d.items() if "state" in key]
+            # self.state.append(self.final_state)
         except Exception as e:
             logging.error(f"Error: No state file found. Exception: {e}")
             raise e
@@ -48,13 +48,12 @@ def handle_get(request):
         category_type = params["category_type"][0]
         pubkey = params["pubkey"][0]
         result = False
-        for state in handler.state:
-            for category in state["state"]["all_category"]:
-                if category["data"]["category_type"] == category_type:
-                    category_elements_child = category["data"]["category_elements_child"]
-                    if check_category_elements_child(pubkey, category_elements_child):
-                        result = True
-                        break
+        for category in handler.state["state"]["all_category"]:
+            if category["data"]["category_type"] == category_type:
+                category_elements_child = category["data"]["category_elements_child"]
+                if check_category_elements_child(pubkey, category_elements_child):
+                    result = True
+                    break
         return JsonResponse({"result": result})
     except Exception as e:
         logging.error(f"Error while handling GET request. Exception: {e}")
@@ -71,7 +70,7 @@ def handle_post(request):
         return HttpResponseBadRequest("Error: Invalid block")
 
     try:
-        newstate = [make_final_state(state, [new_block])[0] for state in handler.state]
+        newstate = make_final_state(handler.state, [new_block])[0]
         handler.state = newstate
     except Exception as e:
         logging.error(f"Error: Invalid block processing. Exception: {e}")
