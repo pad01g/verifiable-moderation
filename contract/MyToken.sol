dd0e9e8fc0055eb576f2b7fc9a2c997c8374035e
tom4649 <tkaaynakiyoshi@gmail.com>
2023-12-07 10:54:20 +0900

    Monor change

--- contract/MyToken.sol
+++ contract/MyToken.sol
@@ -3,7 +3,19 @@ pragma solidity ^0.8.10;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
+
+
 contract MyToken is ERC20 {
+    function stringsEquals(string memory s1, string memory s2) private pure returns (bool) {
+    bytes memory b1 = bytes(s1);
+    bytes memory b2 = bytes(s2);
+    uint256 l1 = b1.length;
+    if (l1 != b2.length) return false;
+    for (uint256 i=0; i<l1; i++) {
+        if (b1[i] != b2[i]) return false;
+    }
+    return true;
+}
     constructor(string memory name, string memory symbol)
         ERC20(name, symbol)
     {
@@ -35,7 +47,7 @@ contract MyToken is ERC20 {
 
     function castVote(bool _voteYes) public {
         require(vote.isOpen, "No vote in progress.");
-        require(vote.target[msg.sender]=="NO", "Already YES.");
+        require(stringsEquals(vote.target[msg.sender], "NO"), "Already YES.");
         require(balanceOf(msg.sender) > 0, "No tokens to vote.");
 
         if (_voteYes) {
